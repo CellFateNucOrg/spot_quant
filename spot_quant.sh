2c43079f35bbcd5fe46abf63bba16107a5d63d0c
dblu13 <dario_bernasconi@icloud.com>
2026-09-07 22:35:58 +0200

    Update spot_quant.sh

--- spot_quant.sh
+++ spot_quant.sh
@@ -13,7 +13,7 @@
 # props_c: Which channel to quantify.
 # marker_c: Channel used for the segmentation.
 # masks_folder: Folder (relative to src_dir) containing the masks.
-# mask_str: String that differentiates the mask from its corresponding image (note that the file extension should be .tif, .npy or .npz).
+# mask_str: Suffix that differentiates the mask from its corresponding image (e.g., _mask.tif).
 # do_3d: Whether to output regionprops for the full mask image.
 # do_mip: Whether to output regionprops for Z-projected masks (MIP).
 # min_planes: Minimum height (number of planes) per mask.
