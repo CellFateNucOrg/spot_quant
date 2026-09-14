198c50b181ab7722df2c58a2d900c0437d610ec9
dblu13 <dario_bernasconi@icloud.com>
2026-09-14 16:59:54 +0200

    Update spot_quant.sh

--- spot_quant.sh
+++ spot_quant.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=spot_quant
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --time=0-12:00:00
-#SBATCH --cpus-per-task=64
-#SBATCH --mem 64GB
+#SBATCH --cpus-per-task=32
+#SBATCH --mem 32GB
 #SBATCH --gres=gpu:1
 
 #### Config ####
@@ -11,7 +11,7 @@
 # src_dir: Directories with images to segment.
 # filter_out: Expression(s) to filter out specific files.
 # props_c: Which channel to quantify.
-# marker_c: Channel used for the segmentation.
+# ref_c: Channel used for the segmentation.
 # masks_folder: Folder (relative to src_dir) containing the masks.
 # mask_str: Suffix that differentiates the mask from its corresponding image (e.g., _mask.tif).
 # do_3d: Whether to output regionprops for the full mask image.
@@ -22,16 +22,16 @@
 
 src_dirs=(
 )
-filter_out=()
+filter_out=(faulty 10x 20x max)
 props_c=1
-marker_c=0
+ref_c=0
 masks_folder=dw/masks
 mask_str='_dw_mask.tif'
-do_3d=true
+do_3d=false
 do_mip=true
-out_folder=z10
-min_planes=10
-pixi_dir=
+out_folder=
+min_planes=
+pixi_dir
 
 #### Script ####
 
@@ -43,7 +43,7 @@ for dir in ${src_dirs[@]}; do
     --src_dir "${dir/Volumes/mnt}" \
     --filter_out "${filter_out[@]}" \
     --props_c "$props_c" \
-    --marker_c "$marker_c" \
+    --ref_c "$ref_c" \
     --masks_folder "$masks_folder" \
     --mask_str "$mask_str" \
     --do_3d "$do_3d" \
